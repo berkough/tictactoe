45d9653b1b63c7c6e093ae4a44bef84fbd30c6ab
Mathew Kane <berkough@gmail.com>
2023-01-10 20:15:59 -0800

    first commit

---
+++ source.js
@@ -0,0 +1,9 @@
+// Tic-Tac-Toe
+
+/**
+ * 
+ * Two players; one X and one O, each take turns placing an X or an O, or capturing a square.
+ * Three squares in a row either vertcally, horizontally, or diagonally.
+ * 
+ */
+
