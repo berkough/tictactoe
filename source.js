d96063e6e7b6804bd25e3e00630534a1c22a1031
Mathew Kane <berkough@gmail.com>
2023-01-13 22:43:10 -0800

    almost there, just need a wincondition check

--- source.js
+++ source.js
@@ -10,32 +10,48 @@ const winConditions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0
 const cells = document.querySelectorAll('.cell'); //An arry of our cells that make up the game board.
 const statusText = document.querySelector('#status'); //Status element that will indicate player turn and display winner message.
 const resetBtn = document.querySelector('#resetButton'); //Button to reset the game back to default.
-let gameState = ['','','','','','','','']; //An array to track the game state and where Xs and Os have been placed.
+let gameState = ['','','','','','','','','']; //An array to track the game state and where Xs and Os have been placed.
 let currentPlayer = 'X'; //X always starts.
 let active = false; //If this boolean vlaue is active then check for winning conditions.
 
+/** Console Log checks. 
 console.log(cells);
 console.log(cells[8]);
-console.log(resetBtn);
+console.log(resetBtn); */
 
-startGame();
+game();
 
-function startGame(){
+function game(){
     cells.forEach(cell => cell.addEventListener('click', clickCell));
     resetBtn.addEventListener('click',resetGame);
-    statusText.textContent = `${currentPlayer}'s Turn`;
+    statusText.innerHTML = `${currentPlayer}'s Turn`;
+    active = true;
 }
 
 function clickCell(){
-
+    const cellId = this.getAttribute("id");
+    console.log('clickCell():'+cellId);
+    if(gameState[cellId]!= '' || !active){
+        return;
+    } else {
+        updateCell(this, cellId);
+    }
+    player();
+    checkWinCondition();
 }
 
 function updateCell(cell, index){
-
+    gameState[index] = currentPlayer;
+    cell.innerHTML = currentPlayer;
 }
 
 function player(){
-
+    if(currentPlayer == 'X'){
+        currentPlayer = 'O';
+    } else if (currentPlayer == 'O'){
+        currentPlayer = 'X';
+    }
+    statusText.innerHTML = `${currentPlayer}'s turn`;
 }
 
 function checkWinCondition(){
@@ -43,8 +59,9 @@ function checkWinCondition(){
 }
 
 function resetGame(){
-    active = false;
-    gameState = ['','','','','','','',''];
     currentPlayer = 'X';
-    return;
+    gameState = ['','','','','','','','',''];
+    statusText.innerHTML = `${currentPlayer}'s Turn`;
+    cells.forEach(cell => cell.innerHTML = '');
+    active = true;
 }
